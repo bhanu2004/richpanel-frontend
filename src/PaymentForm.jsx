342b21bc104d60b9823e042bc18f2f7395e3aa3d
bhanu2004 <75109116+bhanu2004@users.noreply.github.com>
2023-08-11 18:47:04 +0530

    Update PaymentForm.jsx

--- src/PaymentForm.jsx
+++ src/PaymentForm.jsx
@@ -40,7 +40,7 @@ export default function PaymentForm() {
     if(!error) {
         try {
             const {id} = paymentMethod
-            const response = await axios.post("http://localhost:4000/payment", {
+            const response = await axios.post("richpanel-backend-79qm840fe-bhanu2004.vercel.app/payment", {
                 amount: 1000,
                 id
             })
